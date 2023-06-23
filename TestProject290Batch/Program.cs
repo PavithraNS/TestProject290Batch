084bb56932741290293d972aeb56c1bffcc476a3
PavithraNS <nspavi1991@gmail.com>
2023-06-23 09:08:23 +0530

    [Pavithra] Adding Welcome message

---
+++ TestProject290Batch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject290Batch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TestProject");
+            Console.ReadLine();
+        }
+    }
+}
